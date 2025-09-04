9a5bee177d7bf66846f65b843d856cfcdfcf2dce
Moowses <132836745+Moowses@users.noreply.github.com>
2025-09-04 06:45:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const app = express();
 
 
 const ALLOWED_ORIGINS = [
-  'http://localhost:3000',
+  'https://member.dreamtripclub.com',
+  'https://www.dreamtripclub.com',
+  'https://dreamtripclub.com' 
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-    if (ALLOWED_ORIGINS.includes(origin) || origin.endsWith('.dreamtripclub.com')) {
+    if (ALLOWED_ORIGINS.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -46,27 +48,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// token handler.
-const getToken = async () => {
-  const appkey = "41787349523ac4f6";
-  const appSecret = "ftObPzm7cQyv2jpyxH9BXd3vBCr8Y-FGIoQsBRMpeX8";
-
-  try {
-    const res = await axios.post(`${process.env.API_BASE_URL}ClaimVoucher`, {
-      appkey,
-      appSecret
-    });
-
-    if (res.data && res.data.token) {
-      return res.data.token;
-    } else {
-      console.error("Token response missing:", res.data);
-      return null;
-    }
-  } catch (err) {
-    console.error("Error fetching token:", err.message);
-    return null;
-  }
-};
-
-module.exports.getToken = getToken;
