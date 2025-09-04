d8174422b3da3a12e11830bef5cc932608574476
Moowses <132836745+Moowses@users.noreply.github.com>
2025-09-04 07:02:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,11 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  
-  credentials: true
+    
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200
 }));
 
 
